01a88775b31ca156c8bf1724f3f15b56c5a2ee36
aklong <120074791@qq.com>
2022-04-23 23:47:06 +0800

    mysql改为服务器数据库

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 import express from "express";
 import history from "connect-history-api-fallback";
 import path from "path";
+import { select } from "./src/utils/mysql.js";
 const app = express();
 
 // 解析表单数据
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(history({}));
-app.use(express.static(path.resolve("/static")));
+app.use(history({
+  verbose: true,
+  index:'/'
+}));
 
 app.listen(8088, () => {
   console.log("服务器启动！");
 });
+
+select('sys_user').then(res => {
+  console.log(res)
+})
